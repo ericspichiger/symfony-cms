859174d2c104eb345ca23f1d2264cbf3fa98bcd7
Eric Spichiger <e.spichiger@hiveweb.ch>
2022-11-17 20:07:48 +0100

    [Add] Composer : Debug bar

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
